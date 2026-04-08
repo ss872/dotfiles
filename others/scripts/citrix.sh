6abff29589e7c83fbf765e648fd111035ad40900
Shyamal Shah <shah.shyamal.872@gmail.com>
2026-04-07 22:13:59 -0500

    Removed unused configs. updated niri to be latest config

--- others/scripts/citrix.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-# Enable XWayland grab access for Citrix Workspace (Wfica)
-gsettings set org.gnome.mutter.wayland xwayland-grab-access-rules "['Wfica']"
-gsettings set org.gnome.mutter.wayland xwayland-allow-grabs true
